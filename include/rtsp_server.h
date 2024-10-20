744095b392420a8304a21cb06a9eb8c27b40c75b
U-Keun Song <sukeun319@gmail.com>
2024-10-20 10:16:56 +0900

    refactor : add RTSPServer class

--- include/rtsp_server.h
+++ include/rtsp_server.h
@@ -1,10 +1,35 @@
 #ifndef RTSP_SERVER_H
 #define RTSP_SERVER_H
 
-bool start_rtsp_server(const char*);
-void start_streaming();
-void stop_streaming();
-void stop_rtsp_server();
+#include <gst/gst.h>
+#include <gst/rtsp-server/rtsp-server.h>
+#include <mutex>
+
+class RTSPServer {
+public:
+	RTSPServer(const char*);
+	~RTSPServer();
+	void start();
+	void start_streaming();
+	void stop_streaming();
+	void stop();
+
+private:
+	void media_configure(GstRTSPMediaFactory*, GstRTSPMedia*);
+	static void media_configure_callback(GstRTSPMediaFactory* factory, GstRTSPMedia* new_media, gpointer user_data) {
+		RTSPServer* self = static_cast<RTSPServer*>(user_data);
+		self->media_configure(factory, new_media);
+	}
+	static void on_media_unprepared(GstRTSPMedia*, gpointer);
+	
+	GMainLoop* loop;
+	GstRTSPServer* server;
+	GstRTSPMediaFactory* factory;
+	GstRTSPMedia* media;
+	bool is_streaming;
+	std::mutex media_mutex;
+	const char* port;
+};
 
 #endif // RTSP_SERVER_H
 
